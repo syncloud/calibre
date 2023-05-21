edbcfdfe5ea3c4d1bce6fe3e6deebb5630786e01
Boris Rybalkin <ribalkin@gmail.com>
2023-05-21 16:47:56 +0100

    fix docker

--- calibre/build.sh
+++ calibre/build.sh
@@ -7,7 +7,7 @@ docker build --build-arg VERSION=$1 -t syncloud .
 docker create --name=syncloud syncloud
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
-docker export syncloud -o python.tar
+docker export syncloud -o syncloud.tar
 tar xf syncloud.tar
 rm -rf syncloud.tar
 cp ${DIR}/python ${BUILD_DIR}/bin/
