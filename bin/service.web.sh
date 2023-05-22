e0241ff640f93797ff3394fd8071873a00f074c5
Boris Rybalkin <ribalkin@gmail.com>
2023-05-22 21:19:15 +0100

    fix python

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -3,4 +3,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 cd ${DIR}/calibre/web
 export CALIBRE_UNIX_SOCKET=/var/snap/calibre/current/calibre.sock
-exec ${DIR}/calibre/bin/python3 cps.py
+exec ${DIR}/calibre/bin/python cps.py
+
