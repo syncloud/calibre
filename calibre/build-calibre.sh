0d025c63cadba193d1f33eaad7e26fdaa60ca5d9
Boris Rybalkin <ribalkin@gmail.com>
2023-05-24 08:58:25 +0100

    rust only on arm32

--- calibre/build-calibre.sh
+++ calibre/build-calibre.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 VERSION=$1
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
-source "$HOME/.cargo/env"
+
+if [[ $(uname -m) == "armv7l" ]]; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
+  source "$HOME/.cargo/env"
+fi
+
 wget https://github.com/janeczku/calibre-web/releases/download/$VERSION/calibre-web-$VERSION.tar.gz
 tar xf calibre-web-$VERSION.tar.gz
 rm calibre-web-$VERSION.tar.gz
@@ -9,4 +13,7 @@ mv calibre-web-$VERSION web
 wget https://github.com/janeczku/calibre-web/blob/master/library/metadata.db -O web/metadata.db
 wget https://raw.githubusercontent.com/linuxserver/docker-calibre-web/master/root/defaults/app.db -O web/app.db
 cd web && pip install -r requirements.txt
-yes | rustup self uninstall
+
+if [[ $(uname -m) == "armv7l" ]]; then
+  yes | rustup self uninstall
+fi
