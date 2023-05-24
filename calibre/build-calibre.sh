4666cf7ffd54743a117d69374c966b189e4c79fb
Boris Rybalkin <ribalkin@gmail.com>
2023-05-24 20:03:44 +0100

    more requirements

--- calibre/build-calibre.sh
+++ calibre/build-calibre.sh
@@ -12,7 +12,9 @@ rm calibre-web-$VERSION.tar.gz
 mv calibre-web-$VERSION web
 wget https://github.com/janeczku/calibre-web/blob/master/library/metadata.db -O web/metadata.db
 wget https://raw.githubusercontent.com/linuxserver/docker-calibre-web/master/root/defaults/app.db -O web/app.db
-cd web && pip install -r requirements.txt
+cd web
+pip install -r requirements.txt
+pip install -r optional-requirements.txt
 
 if [[ $(uname -m) == "armv7l" ]]; then
   yes | rustup self uninstall
