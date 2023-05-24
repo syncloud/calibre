4a7c16a83a8e9b34580c0304eee9b16245dc8713
Boris Rybalkin <ribalkin@gmail.com>
2023-05-24 18:56:35 +0100

    try debug log

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -4,4 +4,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 cd ${DIR}/calibre/web
 DATA_DIR=/var/snap/calibre/current
 export CALIBRE_UNIX_SOCKET=$DATA_DIR/calibre.sock
+export FLASK_DEBUG=true
 exec ${DIR}/calibre/bin/python cps.py -p $DATA_DIR/app.db
