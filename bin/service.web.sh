9fc41b313af9c247d87bd12456e6056d08845ae5
Boris Rybalkin <ribalkin@gmail.com>
2023-05-25 08:48:15 +0100

    fix db path

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -5,4 +5,5 @@ cd ${DIR}/calibre/web
 DATA_DIR=/var/snap/calibre/current
 export CALIBRE_UNIX_SOCKET=$DATA_DIR/calibre.sock
 export FLASK_DEBUG=true
+export CALIBRE_DBPATH=$DATA_DIR
 exec ${DIR}/calibre/bin/python cps.py -p $DATA_DIR/app.db
