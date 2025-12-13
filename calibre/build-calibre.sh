4fc0009dde5d6d0dce55163373af7aaf92aed88f
Boris Rybalkin <ribalkin@gmail.com>
2025-12-13 15:52:39 +0000

    imagick

--- calibre/build-calibre.sh
+++ calibre/build-calibre.sh
@@ -100,5 +100,6 @@ cp -r /bin ${BUILD_DIR}
 cp -r /usr ${BUILD_DIR}
 cp -r /lib* ${BUILD_DIR}
 cp -r /web ${BUILD_DIR}
+cp -r /ImageMagick ${BUILD_DIR}
 cp ${DIR}/python ${BUILD_DIR}/bin/
 rm -rf ${BUILD_DIR}/usr/src
