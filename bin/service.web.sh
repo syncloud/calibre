0490cee66e20323552b993b5aea0c461110e88b8
Boris Rybalkin <ribalkin@gmail.com>
2025-12-18 17:58:17 +0000

    convert

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -11,7 +11,7 @@ export CALIBRE_LDAP_AUTO_CREATE=true
 export CALIBRE_LDAP_ADMIN_GROUP_FILTER='(&(cn=syncloud)(&(objectClass=posixGroup)(memberUid=%s)))'
 export SECRET_KEY=$(cat $DATA_DIR/secret.key)
 export CALIBRE_BINARIES=$DIR/calibre/opt/calibre
-export CALIBRE_CONVERTER=$DIR/calibre/opt/calibre/ebook-converter
+export CALIBRE_CONVERTER=$DIR/calibre/bin/ebook-convert
 export CALIBRE_UNRAR=$DIR/calibre/bin/unrar
 export CALIBRE_KEPUBIFY=$DIR/calibre/usr/bin/kepubify
 exec ${DIR}/calibre/bin/python cps.py -p $DATA_DIR/app.db
